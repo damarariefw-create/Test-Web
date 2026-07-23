88dafce74e28ba28293b0cbf0ee051eb1e999a76
Damar Arief W <damarariefwtjksn@gmail.com>
2026-07-23 09:08:26 +0700

    Tiket

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ let no_telepon = document.getElementById("no-telepon");
 let tanggal = document.getElementById("tanggal");
 let pembayaran = document.getElementById("pembayaran")
 
+let tnama = document.getElementById("tiket-nama");
+let ttujuan = document.getElementById("tiket-tujuan");
+let tjumlah = document.getElementById("tiket-jumlah");
+let tnotlp = document.getElementById("tiket-notlp");
+let ttanggal = document.getElementById("tiket-tanggal");
+let tpembayaran = document.getElementById("tiket-pembayaran");
 
-submit.addEventListener("click", function () {
-    alert(`${nama.value} telah memesan ${jumlah.value} tiket ke ${tujuan.value} pada tanggal ${tanggal.value} dengan nomor telepon ${no_telepon.value} melalui pembayaran ${pembayaran.value}`);
+
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    tnama.innerHTML = `<b>Nama:</b> ${nama.value}`;
+    ttujuan.innerHTML = `<b>Tujuan:</b> ${tujuan.value}`;
+    tjumlah.innerHTML = `<b>Jumlah Tiket:</b> ${jumlah.value}`;
+    tnotlp.innerHTML = `<b>No Telepon:</b> ${no_telepon.value}`;
+    ttanggal.innerHTML = `<b>Tanggal:</b> ${new Date(tanggal.value).toLocaleDateString('id-ID')}`;
+    tpembayaran.innerHTML = `<b>Pembayaran:</b> ${pembayaran.value}`;
+
 });
