28c0cc7a6e1f822339a145121ddc3ac5f3cab4f4
Damar Arief W <damarariefwtjksn@gmail.com>
2026-07-23 09:31:54 +0700

    Tiket (pembayaran)

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let ttujuan = document.getElementById("tiket-tujuan");
 let tjumlah = document.getElementById("tiket-jumlah");
 let tnotlp = document.getElementById("tiket-notlp");
 let ttanggal = document.getElementById("tiket-tanggal");
+let tharga = document.getElementById("tiket-harga");
 let tpembayaran = document.getElementById("tiket-pembayaran");
 
 
@@ -21,6 +22,27 @@ submit.addEventListener("click", function (event) {
     tjumlah.innerHTML = `<b>Jumlah Tiket:</b> ${jumlah.value}`;
     tnotlp.innerHTML = `<b>No Telepon:</b> ${no_telepon.value}`;
     ttanggal.innerHTML = `<b>Tanggal:</b> ${new Date(tanggal.value).toLocaleDateString('id-ID')}`;
+    let harga = 0;
+    switch (tujuan.value) {
+        case "Jakarta":
+            harga = 100000;
+            break;
+        case "Bandung":
+            harga = 120000;
+            break;
+        case "Surabaya":
+            harga = 130000;
+            break;
+        case "Yogyakarta":
+            harga = 140000;
+            break;
+        case "Semarang":
+            harga = 150000;
+            break;
+    }
+    tharga.innerHTML = `<b>Harga:</b> ${harga * jumlah.value}`;
+
     tpembayaran.innerHTML = `<b>Pembayaran:</b> ${pembayaran.value}`;
 
+    document.getElementById("submit").scrollIntoView({ behavior: "smooth", block: "start" });
 });
